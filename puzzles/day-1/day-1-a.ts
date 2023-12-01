36972cd444e19ca2c91bbcfe9c1cf688e7345fe6
Julien Michel <julienmichel.pro@gmail.com>
2023-12-01 22:48:25 +0100

    day-1-a

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -3,7 +3,17 @@ import chalk from 'chalk';
 
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  return data.reduce((acc, row) => {
+    const numbers = (row.match(/\d/igm) ?? [])
+    if (!numbers.length) {
+      return acc
+    }
+    const first = numbers[0]
+    const last = numbers[numbers.length - 1];
+    console.log({first,last});
+    return acc + parseInt(`${first}${last}`);
+  }, 0)
 }
 
 const answer = await day1a();
