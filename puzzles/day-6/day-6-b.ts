6542f364c90ce14a0e28b5b813663c2bce8d63ec
Julien Michel <julienmichel.pro@gmail.com>
2023-12-06 18:18:30 +0100

    day-6-b

--- puzzles/day-6/day-6-b.ts
+++ puzzles/day-6/day-6-b.ts
@@ -1,9 +1,44 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+class Race {
+  constructor(
+    public readonly time: number,
+    public readonly distance: number,
+  ) {}
+
+  getSolution(): number {
+    const solutions: number[] = [];
+    for (let i = 0; i <= this.time; i++) {
+      const run = i * (this.time - i);
+      if (run > this.distance) {
+        solutions.push(run);
+      }
+    }
+    return solutions.length;
+  }
+}
+
+function getRaces(rows: string[]): Race[] {
+  const [times, distances] = rows
+    .filter((v) => v !== '')
+    .map((row) =>
+      row
+        .split(':')[1]
+        .replace(/\s*/, ' ')
+        .replace('\r', '')
+        .split(' ')
+        .filter((v) => v !== '')
+        .join(''),
+    );
+
+  return [new Race(parseInt(times), parseInt(distances))];
+}
+
 export async function day6b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = await readData(dataPath, false);
+  const races: Race[] = getRaces(data);
+  return races.reduce((acc, race) => acc * race.getSolution(), 1);
 }
 
 const answer = await day6b();
